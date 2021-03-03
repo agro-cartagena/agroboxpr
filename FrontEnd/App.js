cd8e31d3a2a20559a5e0dd97065cdeff2f8ade8e
hectordael371 <hector.jimenez@upr.edu>
2021-03-02 22:45:13 -0400

    Added RegisterScreen

--- FrontEnd/App.js
+++ FrontEnd/App.js
@@ -125,11 +125,13 @@
 import React from 'react'
 import { ScrollView, StyleSheet } from 'react-native';
 import LoginScreen from './screens/LoginScreen/LoginScreen'
+import RegisterScreen from './screens/RegisterScreen/RegisterScreen'
 
 class App extends React.Component {
   render() {
     return (
-      <LoginScreen/>
+      // <LoginScreen/>
+      <RegisterScreen/>
     )
   }
 }

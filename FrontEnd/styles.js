70d7e2b60bd20a798caea1d641416dc59abff813
hectordael371 <hector.jimenez@upr.edu>
2021-03-11 00:17:07 -0400

    Fixed FormInput styles location. Ready to integrate with backend.

--- FrontEnd/styles.js
+++ FrontEnd/styles.js
@@ -17,19 +17,6 @@ const styles = StyleSheet.create({
         height: '100%'
     },
 
-    textEntry: {
-        width: '80%',
-        height: 45,
-
-        textAlign: 'center',
-        borderRadius: 100,
-        borderWidth: 1,
-        borderColor: 'lightgray',
-
-        // backgroundColor: '#E5E5E5',
-        backgroundColor: 'white',
-    },
-
     text: {
         color: 'white',
         fontSize: 16

61ea277dc360ff66de7de2138528aa3378c35f08
gabistgo <gabriela.santiago18@upr.edu>
2021-04-12 12:47:00 -0400

    Added my changes to cart screen

--- FrontEnd/screens/CartScreen/CartScreen.js
+++ FrontEnd/screens/CartScreen/CartScreen.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { ScrollView, View, Image, Text } from 'react-native'
 import BoxCard from '../../components/BoxCard/BoxCard'
+import PlusMinus from '../../components/PlusMinus/PlusMinus'
 
 import CartService from '../../services/CartService'
 import styles from './CartScreenStyleSheet'
@@ -10,14 +11,36 @@ import Logo from '../../components/Logo/Logo'
 import Button from '../../components/Button/Button'
 import cart_list from '../../db_mockup/cart.db'
 
-const CartScreen = () => {
+import ProductService from '../../services/ProductService'
+import BoxService from '../../services/BoxService'
+
+const CartScreen = (props) => {
     let cart = []
+    
+    const [boxData, setBoxData] = React.useState({})
+   
+    // React.useEffect(() => {
+    //     async function fetchData() {
+    //         let _box = {
+    //             ...props, 
+    //             box_quantity: 1,
+    //             box_accumulated_price: _isBuildYourBox ? 0 : props.params.box_price,
+    //             box_content:  _isBuildYourBox ? {} : await BoxService.instance.getBoxContent()
+    //         }
+
+    //         setBoxData(_box)
+    //         if (_isBuildYourBox)
+    //             setProductCatalog(await ProductService.instance.getProductCatalog())
+    //     }
+
+    //     fetchData()
+    // }, []);
 
     const loadCart = async () => {
         cart_list.forEach((item) => {
             // alert(JSON.stringify(item))
             cart.push(
-                <View style={styles.itemContainer}>
+                <View style={styles.itemContainer} key={item.box_id}>
                     <View key={item.box_name} style={styles.cardContainer}>
                         <BoxCard
                             id={item.box_id}
@@ -25,10 +48,18 @@ const CartScreen = () => {
                             image={item.box_image}
                         />
                     </View>
-
-                    <Text>${item.box_price} </Text>
-                    <Text>x{item.quantity} </Text>
-                    <Text>= ${item.box_price*item.quantity}</Text>
+                    
+                    <View style={styles.plusminus}>
+                        <PlusMinus 
+                             onMinus={() => { if(boxData.box_quantity > 1) setBoxData({ ...boxData, box_quantity: boxData.box_quantity -= 1})}}
+                             onPlus={()=>{ if(boxData.box_quantity < 100) setBoxData({ ...boxData, box_quantity: boxData.box_quantity += 1})}}
+                             placeholder={ boxData.box_quantity }
+                        />
+                    </View>
+                    
+                    {/* <Text>${item.box_price} </Text> */}
+                    {/* <Text>x{item.quantity} </Text> */}
+                    <Text style={{fontWeight: 'bold', fontSize: 15}}> = ${item.box_price*item.quantity}</Text>
                 </View>
 
 
@@ -49,12 +80,12 @@ const CartScreen = () => {
             </View>
 
             <Text style={[global_styles.text, styles.text]}>Total de compra: 
-                <Text style={{fontWeight: 'bold'}}> ${80}</Text>
+                <Text style={{fontWeight: 'bold', color: '#EAC71D'}}> ${80}</Text>
             </Text>
 
             <View style={styles.buttonContainer}>
                 <Button
-                    text="Confirmar"
+                    text="Pagar"
                 />
             </View>
         </ScrollView>

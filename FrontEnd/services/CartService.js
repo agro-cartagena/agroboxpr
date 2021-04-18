d681da988f33ce7e9b344e9112f7fdb86aabd59f
hectordael371 <hector.jimenez@upr.edu>
2021-04-18 12:50:37 -0400

    Fixed cart update issue. Minor bugs remaining. Added Trash icons.

--- FrontEnd/services/CartService.js
+++ FrontEnd/services/CartService.js
@@ -7,6 +7,19 @@ export default class CartService {
     constructor() { }
 
     addToCart(item) {
+        const randomize = (string) => {
+            let arr = string.split(""),
+                size = arr.length;
+    
+            for(let currentIndex = (size-1); currentIndex>0; currentIndex--) {
+                let randomIndex = Math.floor(Math.random() * (currentIndex + 1));
+                [arr[currentIndex], arr[randomIndex]] = [arr[randomIndex], arr[currentIndex]]
+            }
+
+            return arr.join("");
+        }
+
+        item._id = randomize(item._id)
         this._cart.push(item)
     }
 

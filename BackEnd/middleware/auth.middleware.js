a31edf165af96661761ec8133b92500b17ce339d
Alberto J. Perez Roman <alberto.perez8@upr.edu>
2021-04-18 23:04:03 -0400

    began integration of jwt

--- BackEnd/middleware/auth.middleware.js
+++ BackEnd/middleware/auth.middleware.js
@@ -1,82 +1,111 @@
-
-const jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken')
 const { authService } = require('../services')
 
-const { getAccessToken } = authService;
+const { getAccessToken } = authService
 
 const auth = async (req, res, next) => {
-    console.log("Starting")
-    try {
-        const token = req.header("x-access-token");
+	console.log('Starting')
+	try {
+		const token = req.header('x-access-token')
 
-        if (!token) {
-            return res.status(403).json({ msg: "No authentication token, authorization denied" });
-        }
+		if (!token) {
+			return res
+				.status(403)
+				.json({ msg: 'No authentication token, authorization denied' })
+		}
 
-        const verified = jwt.verify(token, process.env.JWT_SECRET, function (err, decoded) {
-            if (!err) return decoded;
+		const verified = jwt.verify(
+			token,
+			process.env.JWT_SECRET,
+			function (err, decoded) {
+				if (!err) return decoded
 
-            console.log("Error", err)
+				console.log('Error', err)
 
-            return null;
-        });
+				return null
+			}
+		)
 
-        if (!verified) {
-            return res.status(401).json({ msg: "Token verification failed, authorization denied" });
-        }
+		if (!verified) {
+			return res
+				.status(401)
+				.json({ msg: 'Token verification failed, authorization denied' })
+		}
 
-        //Refresh token and send to client
-        // let newToken = await getAccessToken(verified.id)
-        // res.setHeader("x-auth-token", newToken);
-        console.log("Verified object: ", verified)
+		//Refresh token and send to client
+		// let newToken = await getAccessToken(verified.id)
+		// res.setHeader("x-auth-token", newToken);
+		console.log('Verified object: ', verified)
 
-        req.userId = verified.userId;
-        req.userRole = verified.role;
-        next();
-    } catch (err) {
-        res.status(500).json({ error: err.message })
-    }
-
-};
+		req.userId = verified.userId
+		req.userRole = verified.role
+		next()
+	} catch (err) {
+		res.status(500).json({ error: err.message })
+	}
+}
 
 const adminAuth = async (req, res, next) => {
-    console.log("Starting")
-    try {
-        const token = req.header("x-access-token");
+	console.log('Starting')
+	try {
+		const token = req.header('x-access-token')
 
-        if (!token) {
-            return res.status(403).json({ msg: "No authentication token, authorization denied" });
-        }
+		if (!token) {
+			return res
+				.status(403)
+				.json({ msg: 'No authentication token, authorization denied' })
+		}
 
-        const verified = jwt.verify(token, process.env.JWT_SECRET, function (err, decoded) {
-            if (!err) return decoded;
+		const verified = jwt.verify(
+			token,
+			process.env.JWT_SECRET,
+			function (err, decoded) {
+				if (!err) return decoded
 
-            console.log("Error", err)
+				console.log('Error', err)
 
-            return null;
-        });
+				return null
+			}
+		)
 
-        if (!verified) {
-            return res.status(401).json({ msg: "Token verification failed, authorization denied" });
-        }
+		if (!verified) {
+			return res
+				.status(401)
+				.json({ msg: 'Token verification failed, authorization denied' })
+		}
 
-        if (verified.role != "admin") {
-            return res.status(403).json({ msg: "Not authrorized for request." });
-        }
+		if (verified.role != 'admin') {
+			return res.status(403).json({ msg: 'Not authrorized for request.' })
+		}
 
-        //Refresh token and send to client
-        // let newToken = await getAccessToken(verified.id)
-        // res.setHeader("x-auth-token", newToken);
+		//Refresh token and send to client
+		// let newToken = await getAccessToken(verified.id)
+		// res.setHeader("x-auth-token", newToken);
 
-        req.userId = verified.userId;
-        next();
-    } catch (err) {
-        res.status(500).json({ error: err.message })
-    }
+		req.userId = verified.userId
+		next()
+	} catch (err) {
+		res.status(500).json({ error: err.message })
+	}
+}
 
-};
+const decodeUserData = async (req, res, next) => {
+	try {
+		const token = req.header('x-access-token')
+		if (!token) {
+			return res.status(403).json({ msg: 'No user logged in' })
+		}
+        const userData = jwt.decode(token)
+        console.log(userData);
+        req.userId = userData.userId
+        next()
+	} catch (err) {
+		res.status(500).json({ error: err.message })
+	}
+}
 
 module.exports = {
-    auth,
-    adminAuth
-};
+	auth,
+	adminAuth,
+    decodeUserData
+}

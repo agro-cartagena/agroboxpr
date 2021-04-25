63f44112ff91a30c16e58a448d699808d5eaf666
Alberto <36648131+AlbertoPerez8@users.noreply.github.com>
2021-04-25 16:39:49 -0400

    Update order.js

--- BackEnd/routes/order.js
+++ BackEnd/routes/order.js
@@ -14,6 +14,6 @@ router.put('/:id', orderController.update) //update order
 router.get('/', orderController.getAll) //get all orders in system
 
 
-router.post('/manage/:id', orderController.manage)
+router.put('/manage/:id', orderController.manage)
 
 module.exports = router

fb3e7cc00c3fa6b29423dd6bbf398555ebf283a1
hectordael371 <hector.jimenez@upr.edu>
2021-04-26 17:36:53 -0400

    Added authentication middleware to order route.

--- BackEnd/routes/order.js
+++ BackEnd/routes/order.js
@@ -8,7 +8,7 @@ router.post('/', authentication.auth, orderController.postOrder) //Submit order 
 
 //Gets
 router.get('/id/:id', orderController.getById) //get order using orderID
-router.get('/user/:id', orderController.getUserOrders) //get all orders from a user
+router.get('/user', authentication.auth, orderController.getUserOrders) //get all orders from a user
 router.get('/status', orderController.getByStatus) //get all orders sorted by status
 router.get('/city/:city', orderController.getByCity) //get order by municipality
 router.get('/', orderController.getAll) //get all orders in system

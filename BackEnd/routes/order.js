6aa9c02f9479963eaca7e31fe718ce4779df07e5
Alberto <36648131+AlbertoPerez8@users.noreply.github.com>
2021-04-26 15:07:15 -0400

    Update order.js
    got order routes up to date, after a commit tracking issue

--- BackEnd/routes/order.js
+++ BackEnd/routes/order.js
@@ -1,20 +1,20 @@
 var express = require('express')
 var router = express.Router()
 const authentication = require('../middleware/auth.middleware')
-var { validateEntity } = require('../middleware/dataValidation.middleware')
-var { orderBodySchema } = require('../middleware/validators.middleware')
 const { orderController } = require('../controllers')
 
-
-
+//Posts
 router.post('/', authentication.auth, orderController.postOrder) //Submit order (auth)
+
+//Gets
 router.get('/id/:id', orderController.getById) //get order using orderID
 router.get('/user/:id', orderController.getUserOrders) //get all orders from a user
 router.get('/status', orderController.getByStatus) //get all orders sorted by status
 router.get('/city/:city', orderController.getByCity) //get order by municipality
 router.get('/', orderController.getAll) //get all orders in system
 
-
+//Puts
+router.put('/:id', orderController.update) //update order
 router.put('/manage/:id', orderController.manage)
 
 module.exports = router
